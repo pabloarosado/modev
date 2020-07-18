e65b05c897bbf2f68324a045b4d606ebb65082a9
Pablo A. Rosado <pabloarosado@gmail.com>
2020-07-18 19:26:05 +0200

    Implement k fold playground and n tests splitting

--- modev/default.py
+++ modev/default.py
@@ -5,13 +5,15 @@ from modev import evaluation
 
 default_random_state = 1000
 default_test_fraction = 0.2
-default_n_folds = 4
+default_test_n_sets = 1
+default_playground_n_folds = 4
 
 default_experiment = {'data_function': etl.load_local_file,
                       'data_pars': {'data_file': None, 'random_state': default_random_state},
-                      'validation_function': validation.folding,
-                      'validation_pars': {'type': 'k_fold', 'test_fraction': default_test_fraction,
-                                          'n_folds': default_n_folds,
+                      'validation_function': validation.k_fold_playground_n_tests_split,
+                      'validation_pars': {'test_fraction': default_test_fraction,
+                                          'test_n_sets': default_test_n_sets,
+                                          'playground_n_folds': default_playground_n_folds,
                                           'random_state': default_random_state},
                       'evaluation_function': evaluation.evaluate_predictions,
                       'evaluation_pars': {'metrics': ['precision', 'accuracy']},
