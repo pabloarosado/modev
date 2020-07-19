c25f41d51d5e09aebcc4e18b5d4cdda4a88ea90f
Pablo A. Rosado <pabloarosado@gmail.com>
2020-07-19 18:39:07 +0200

    Import raw experiments as modules instead of json files

--- modev/default.py
+++ modev/default.py
@@ -8,16 +8,21 @@ default_test_fraction = 0.2
 default_test_n_sets = 1
 default_playground_n_folds = 4
 
-default_experiment = {'data_function': etl.load_local_file,
-                      'data_pars': {'data_file': None, 'random_state': default_random_state},
+default_experiment = {'load_function': etl.load_local_file,
+                      'load_pars': {'data_file': None,
+                                    'random_state': default_random_state,
+                                    },
                       'validation_function': validation.k_fold_playground_n_tests_split,
                       'validation_pars': {'test_fraction': default_test_fraction,
                                           'test_n_sets': default_test_n_sets,
                                           'playground_n_folds': default_playground_n_folds,
-                                          'random_state': default_random_state},
+                                          'random_state': default_random_state,
+                                          },
                       'evaluation_function': evaluation.evaluate_predictions,
-                      'evaluation_pars': {'metrics': ['precision', 'accuracy']},
+                      'evaluation_pars': {'metrics': ['precision', 'accuracy'],
+                                          },
                       'exploration_function': exploration.grid_search,
-                      'exploration_pars': {'do_not_expand': None},
+                      'exploration_pars': {'do_not_expand': None,
+                                           },
                       'approaches': [{}],
                       }
