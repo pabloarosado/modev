74e2ae06bd238d8eefd0f504686cdd0d482aae7c
Pablo A. Rosado <pabloarosado@gmail.com>
2020-07-28 20:21:12 +0200

    Change package name in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="modev-pablorosado",
+    name="modev",
     version="0.0.1",
     author="Pablo Rosado",
     author_email="pabloarosado@gmail.com",
